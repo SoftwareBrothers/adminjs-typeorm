e5641d2133a98a1f3f73eb1ed5a22c57437584dd
Seven Du <whine.whitish.0u@icloud.com>
2023-07-10 13:18:21 +0800

    feat: support raw custom filters

--- src/utils/filter/custom-filter.parser.ts
+++ src/utils/filter/custom-filter.parser.ts
@@ -1,23 +1,56 @@
-import { FilterParser } from './filter.types.js'
+import { FilterElement } from 'adminjs';
+import { FilterParser } from './filter.types.js';
+
+const isCustomFilter = (filter: any) => filter?.custom;
+const isRawCustomFilter = (filter: FilterElement) =>
+  isCustomFilter(filter.value);
+
+const isParserForType: FilterParser['isParserForType'] = (
+  filter: FilterElement,
+) => isCustomFilter(filter) || isRawCustomFilter(filter);
+
+const readFilterValue = (filter: FilterElement): any => {
+  if (isRawCustomFilter(filter)) {
+    return filter.value['custom'];
+  }
+
+  return filter['custom'];
+};
+
+const parse: FilterParser['parse'] = (
+  filter: FilterElement,
+  filterKey: string,
+) => ({ filterKey, filterValue: readFilterValue(filter) });
 
 /**
  * It wasn't possible to pass raw filters to adapters with AdminJS
- * This solution allows you to pass custom filters to typeorm adapter modyfing list handler
  *
- * In your custom list handler modify creating filters in this way:
+ * ### In your custom handler modify creating filter
  *
+ * ```ts
+ * const filter = new Filter(filters, resource);
+ *
+ * filter.filters[YOUR_FILTER_KEY] = {
+ *     custon: In([1, 2, 3])
+ * };
  * ```
- * // That makes `Filter` class to create proper filter object.
- * filters[propertyToFilterBy] = 1;
- * const filter = await new Filter(filters, resource).populate();
- * // This parser recognizes `custom` field and passes the value directly to typeorm
- * filter.filters[propertyToFilterBy].custom = In([1,2,3]);
  *
+ * ### In AdminJS raw filter will be passed as:
+ *
+ * ```ts
+ * const testResource = {
+ *     resource: YOUR_RESOURCE,
+ *     actions: {
+ *         list: {
+ *            before(request, context) {
+ *               request.query['filters.YOUR_FILTER_KEY~~custom'] = In(1, 2, 3),
+ *            },
+ *         }
+ *     },
+ * };
+ * ```
  */
 export const CustomParser: FilterParser = {
-  isParserForType: (filter) => (filter as any)?.custom,
-  parse: (filter, fieldKey) => ({
-    filterKey: fieldKey,
-    filterValue: (filter as any)?.custom,
-  }),
-}
+  isParserForType,
+  parse,
+};
